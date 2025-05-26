cf62b08c8f228b06e7aced4864529087654016ea
dariomedina10 <dariomedina10@gmail.com>
2025-05-26 03:03:44 -0400

    estilos de boton

--- src/componentes/Boton.jsx
+++ src/componentes/Boton.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import '../hojaestilos/boton.css';
 
 function MyButton() {
   return (
-    <button>Soy un botón</button>
+    //<button>Soy un botón</button>
+    <button class="boton">Soy un botón</button>
   );
 }
 export default MyButton
